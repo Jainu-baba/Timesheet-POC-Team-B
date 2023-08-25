1203f9827bf3f1576a072c0ecb8826d31bcbdc91
Satuluri Baba <satuluri.baba@globallogic.com>
2023-08-25 12:20:04 +0530

    manager-screen

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(rootReducer);
+//const store = createStore(rootReducer);
 root.render(
-  <React.StrictMode>
-   <Provider store={store}>
+  
  <App />
-</Provider>
-  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
