e24bd4a864efe8f001c2513e803f67f49a32dc5c
Bhargavijk <Pothavaram.Bhargavi@jktech.com>
2023-09-05 10:44:44 +0530

    changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./Home.css"
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
+import image from '../../../src/image.png';
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <div className="mainButton">
-        <Button variant="contained" color="success" size="large" onClick={() => navigate("/employee")} className="button">Employee</Button>
+       <div className="container" > 
+      <img src={image} alt="Snow"/>
+      <div className="btn">
+        <Button variant="contained" color="success" size="large" onClick={() => navigate("/employee")} >Employee</Button>
         <Button variant="contained" color='success' size="large" onClick={() => navigate("/manager")}>Manager</Button>
       </div>
-
+      </div> 
     </div>
   );
 };
